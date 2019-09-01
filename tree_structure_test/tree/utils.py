da97005a27f7c332e72e862ba60d1bd9174292ad
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-09-01 19:46:13 +0300

    Added dispatch method for checking csrf tocken

--- tree_structure_test/tree/utils.py
+++ tree_structure_test/tree/utils.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Category
@@ -9,22 +10,23 @@ from .serializers import CategorySerializer
 class CategoryMixin:
     model = None
 
-    def get_serializable_node(self, node):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(CategoryMixin, self).dispatch(request, *args, **kwargs)
+
+    def _get_serializable_node(self, node):
         s_node = {"id": node.id, "name": node.name}
         return s_node
 
     def get(self, request, id):
         obj = get_object_or_404(Category, id__iexact=id)
-        children = [self.get_serializable_node(node) for node in obj.get_children()]
-        siblings = [self.get_serializable_node(node) for node in obj.get_siblings()]
-        parents = [self.get_serializable_node(node) for node in obj.get_ancestors()]
-
+        children = [self._get_serializable_node(node) for node in obj.get_children()]
+        siblings = [self._get_serializable_node(node) for node in obj.get_siblings()]
+        parents = [self._get_serializable_node(node) for node in obj.get_ancestors()]
         data = {"id": obj.id, "name": obj.name,
                 "parents": parents, "children": children, "siblings": siblings}
-
         return JsonResponse(data, json_dumps_params={"indent": 4}, status=200)
 
-    @csrf_exempt
     def post(self, request):
         data = JSONParser().parse(request)
         serializer = CategorySerializer(data=data)
