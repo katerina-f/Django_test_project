53c470a4d269237639e2eba272e2606544dc943b
Katerina Frolova <ekaterina.frolova40@gmail.com>
2019-09-04 12:00:49 +0300

    Change the logic - change model to Django model, deleted mptt module

--- tree_structure_test/tree/models.py
+++ tree_structure_test/tree/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from mptt.models import MPTTModel, TreeForeignKey
 
 
-class Category(MPTTModel):
+class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    parent = models.ForeignKey('self', blank=True, null=True, on_delete='CASCADE',
+                               related_name='children')
 
-    class MPTTMeta:
-        order_insertion_by = ['name']
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
--- tree_structure_test/tree/utils.py
+++ tree_structure_test/tree/utils.py
@@ -4,7 +4,6 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Category
-from .serializers import CategorySerializer
 
 
 class CategoryMixin:
@@ -18,19 +17,47 @@ class CategoryMixin:
         s_node = {"id": node.id, "name": node.name}
         return s_node
 
+    def _load_catalog(self, data, parent):
+        category, created = Category.objects.get_or_create(name=data.get('name'))
+        try:
+            category.parent = category
+        except ValueError:
+            error = {'error': 'invalid data, must be "Category" instance'}
+            return JsonResponse(error, json_dumps_params={"indent": 4}, status=201, safe=False)
+
+        parent = category
+        children = data.get('children')
+        if children:
+            [self._load_catalog(child, parent) for child in children]
+        category.save()
+
+    def get_ancestors(self, obj):
+        ancestors = []
+        ancestor = obj
+        while ancestor:
+            parent = ancestor.parent
+            ancestors.insert(0, parent)
+            ancestor = parent
+        return ancestors
+
+    def get_children(self, obj):
+        children = self.model.objects.filter(parent=obj)
+        return children
+
+    def get_siblings(self, obj):
+        siblings = self.model.objects.filter(parent=obj.parent).exclude(id=obj.id)
+        return siblings
+
     def get(self, request, id):
-        obj = get_object_or_404(Category, id__iexact=id)
-        children = [self._get_serializable_node(node) for node in obj.get_children()]
-        siblings = [self._get_serializable_node(node) for node in obj.get_siblings()]
-        parents = [self._get_serializable_node(node) for node in obj.get_ancestors()]
+        obj = get_object_or_404(self.model, id__iexact=id)
+        parents = [self._get_serializable_node(node) for node in self.get_ancestors(obj) if node]
+        children = [self._get_serializable_node(node) for node in self.get_children(obj)]
+        siblings = [self._get_serializable_node(node) for node in self.get_siblings(obj)]
         data = {"id": obj.id, "name": obj.name,
                 "parents": parents, "children": children, "siblings": siblings}
         return JsonResponse(data, json_dumps_params={"indent": 4}, status=200)
 
     def post(self, request):
         data = JSONParser().parse(request)
-        serializer = CategorySerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, json_dumps_params={"indent": 4}, status=201)
-        return JsonResponse(serializer.errors, json_dumps_params={"indent": 4}, status=400)
+        self._load_catalog(data, parent=None)
+        return JsonResponse(data, json_dumps_params={"indent": 4}, status=201, safe=False)
